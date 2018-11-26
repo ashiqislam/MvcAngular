223433eb78fca5e775537c0869aa79f5f7f19e10
Ashiq <ashiq08401@gmail.com>
2018-11-25 19:10:19 -0500

    Upated AuthController

--- AngularApp/Controllers/AuthController.cs
+++ AngularApp/Controllers/AuthController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AngularApp.Data;
+using AngularApp.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AngularApp.Controllers
@@ -11,5 +14,34 @@ namespace AngularApp.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthController(UserManager<User> userMananger, SignInManager<User> signinManager)
+        {
+            _userManager = userMananger;
+            _signInManager = signinManager;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Test succeeded";
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserVM user)
+        {
+
+            User newUser = new User { UserName = user.UserName };
+            var result = await _userManager.CreateAsync(newUser, user.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { Msg = "Registration succeeded", User = newUser.UserName, ID = newUser.Id });
+            }
+            return BadRequest(result);
+
+        }
+
     }
 }
